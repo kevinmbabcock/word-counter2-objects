1718e404095ace050c79da86ced43b577392882d
Guest User <Guest@epicodus-10.local>
2017-10-31 13:48:04 -0700

    ?

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,22 @@ $(document).ready(function() {
 
     var countedWords = words.reduce(function (allWords, word) {
       if (word in allWords) {
+        console.log(allWords);
         allWords[word]++;
       }
       else {
         allWords[word] = 1;
       }
+      //console.log(allWords);
       return allWords;
-    }, {});
-
+    },
+    {});
 
       var keyWords = Object.keys(countedWords);
-      var valueNumbes = Object.values(countedWords);
+      var valueNumbers = Object.values(countedWords);
       for (index=0; index < keyWords.length; index++) {
-        $("#list").append("<li>" + keyWords[index] + ", " + valueNumbes[index] + "</li>");
+        $("#list").append("<li>" + keyWords[index] + ", " + valueNumbers[index] + "</li>");
       }
-
-
 
     $(".wordsList").show();
   });
